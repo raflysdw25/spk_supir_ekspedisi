5043e98efde0fc620abeeb98394da4e7011130a6
raflysdw <raflysdw25@gmail.com>
2019-12-29 12:18:43 +0700

    halaman admin sm halaman supir untuk tampilin data

--- halaman_admin.php
+++ halaman_admin.php
@@ -1,3 +1,16 @@
+<?php 
+  require "functions.php";
+
+  // Menampilkan halaman admin
+  $query = "SELECT tr.id_transaksi, p.nama_pemesan, tr.jumlah_pesanan, tr.alamat_pengambilan,
+            tr.alamat_tujuan, tr.jenis_pengiriman, tr.tanggal_sampai, tr.status_pengiriman, k.nama_krwn
+            FROM transaksi_pemesanan tr JOIN pemesan p ON (tr.id_pemesan = p.id_pemesan) JOIN karyawan k ON
+            (tr.nik_krwn = k.nik_krwn)";
+  $result = query($query);
+  $jmlhpesanan = count($result);
+  
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,37 +25,79 @@
 
 </head>
 <body>
-    <div class="container my-4">
+    <div class="container">
+      <div class=" row my-4">
 
-    <div class="title my-2">
-      <h1 class="display-4">Selamat Datang, Admin Super</h1>
-      <p class="text-muted text-monospace">
-        Data dibawah ini adalah data pesanan yang dibuat oleh seluruh cabang
-      </p>
-    </div>
-    
-    <a href="" class="btn btn-danger float-right my-2">
-      <span class="fa fa-power-off"></span> Logout
-    </a>
-
-    <table class="table table-bordered table-responsive-sm">
-      <thead>
-        <tr>
-          <th scope="col">NO</th>
-          <th scope="col">NAMA PESANAN</th>
-          <th scope="col">JUMLAH PESANAN</th>
-          <th scope="col">ALAT PEMESAN</th>
-          <th scope="col">ALAMAT TUJUAN</th>
-          <th scope="col">JENIS PENGIRIMAN</th>
-          <th scope="col">TANGGAL PENGIRIMAN</th>
-          <th scope="col">STATUS PENGIRIMAN</th>
-          <th scope="col">PENGIRIMAN UNIT</th>
-        </tr>
-      </thead>
-      <tbody>
+        <div class="col-lg-7 my-2">
+          <h1 class="">Selamat Datang, Admin Super</h1>
+          <p class="text-muted text-monospace">
+            Data dibawah ini adalah data pesanan yang dibuat oleh seluruh cabang
+          </p>
+        </div>
         
-      </tbody>
-    </table>
+        <div id="button" class=" col-lg-5 my-3 text-right">
+          <a href="" class="btn btn-primary p-3">
+            <span class="fa fa-plus"></span> Tambah Pegawai
+          </a>
+
+          <a href="" class="btn btn-danger p-3">
+            <span class="fa fa-power-off"></span> Logout
+          </a>
+        </div>
+
+        <br>
+      </div>
+    </div>  
+    
+
+  <main id="content">
+
+    <?php if($jmlhpesanan == 0): ?>
+      <div class="container alert alert-danger text-center">
+        Tidak terdapat pesanan
+      </div>
+    <?php die; endif; ?>
+    
+    <div id="table-data" class="container">
+      
+      <table class="table table-bordered table-responsive-sm">
+        <thead>
+          <tr>
+            <th scope="col">NO</th>
+            <th scope="col">NAMA PEMESAN</th>
+            <th scope="col">JUMLAH PESANAN</th>
+            <th scope="col">ALAMAT PENGAMBILAN</th>
+            <th scope="col">ALAMAT TUJUAN</th>
+            <th scope="col">JENIS PENGIRIMAN</th>
+            <th scope="col">TANGGAL SAMPAI</th>
+            <th scope="col">PENGIRIM UNIT</th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php $no = 1;
+            foreach ($result as $rslt): ?>
+            <tr>
+              <td><?= $no++; ?></td>
+              <td><?= $rslt["nama_pemesan"]; ?></td>
+              <td><?= $rslt["jumlah_pesanan"]; ?></td>
+              <td><?= $rslt["alamat_pengambilan"]; ?></td>
+              <td><?= $rslt["alamat_tujuan"]; ?></td>
+              <td><?= $rslt["jenis_pengiriman"]; ?></td>
+              <td><?= $rslt["tanggal_sampai"]; ?></td>
+              <td>
+                <?php if($rslt["nik_krwn"] == "NULL"):?>
+                    <a href="#" class="btn">Tentukan Supir</a>
+                <?php else:?>
+                  <p><?= $rslt["nama_krwn"]; ?></p>
+                <?php endif;?>
+              </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+      </table>
+      
     </div>
+
+  </main>
 </body>
 </html>
--- halaman_supir.php
+++ halaman_supir.php
@@ -1,3 +1,20 @@
+<?php
+  require "functions.php";
+
+  // nik_krwn dengan jabatan supir
+
+
+  // Menampilkan halaman admin
+  $query = "SELECT tr.id_transaksi, p.nama_pemesan, tr.jumlah_pesanan, tr.alamat_pengambilan,
+            tr.alamat_tujuan, tr.jenis_pengiriman, tr.tanggal_sampai, tr.status_pengiriman, k.nama_krwn
+            FROM transaksi_pemesanan tr JOIN pemesan p ON (tr.id_pemesan = p.id_pemesan) JOIN karyawan k ON
+            (tr.nik_krwn = k.nik_krwn)";
+  $result = query($query);
+  $jmlhpesanan = count($result);
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,43 +30,85 @@
 </head>
 <body>
     <div class="container my-5">
-      <h1 class="display-4">Selamat Datang, Rafly Sadewa</h1>
-      <p class="text-muted text-monospace">
-        Dibawah ini adalah data pesanan yang anda dapatkan
-      </p>
-      <form action="" class="form-inline float-right">
-        <select name="" id="" class="custom-select mr-2">
-          <option value="Available">Available</option>
-          <option value="Not Available">Not Available</option>
-        </select>
+      <div class="row my-4">
+          <div class="col-lg-7 my-2">
+            <h1 class="display-4">Selamat Datang, Rafly Sadewa</h1>
+            <p class="text-muted text-monospace">
+              Dibawah ini adalah data pesanan yang anda dapatkan
+            </p>
+          </div>
+          
+          <div class="col-lg-5 my-3">
+            <form action="" class="form-inline float-right">
+              <select name="" id="" class="custom-select mr-2">
+                <option value="Available">Available</option>
+                <option value="Not Available">Not Available</option>
+              </select>
 
-        <button class="btn btn-primary mr-2">Set Status</button>
+              <button class="btn btn-primary mr-2">Set Status</button>
 
-        <a class="btn btn-danger text-white float-right my-2">
-            <i class="fa fa-power-off"></i> Logout
-        </a>
-      </form>
-
-      
-
-      
-
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">ID Transaksi</th>
-            <th scope="col">Pemesan</th>
-            <th scope="col">Jumlah Pesanan</th>
-            <th scope="col">Alamat Pengambilan</th>
-            <th scope="col">Alamat Tujuan</th>
-            <th scope="col">Jenis Pengiriman</th>
-            <th scope="col">Tanggal Sampai</th>
-            <th scope="col">Status Pengiriman</th>
-          </tr>
-        </thead>
-      </table>
+              <a class="btn btn-danger text-white float-right my-2">
+                  <i class="fa fa-power-off"></i> Logout
+              </a>
+            </form>
+          </div>
+      </div>
     </div>
+      
+    <main id="content">
 
+      <?php if($jmlhpesanan == 0): ?>
+        <div class="container alert alert-danger text-center">
+          Tidak terdapat pesanan
+        </div>
+      <?php die; endif; ?>
+
+      <div id="table-data" class="container">
+
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">NO</th>
+              <th scope="col">Pemesan</th>
+              <th scope="col">Jumlah Pesanan</th>
+              <th scope="col">Alamat Pengambilan</th>
+              <th scope="col">Alamat Tujuan</th>
+              <th scope="col">Jenis Pengiriman</th>
+              <th scope="col">Tanggal Sampai</th>
+              <th scope="col">Status Pengiriman</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <?php $no = 1;
+            foreach($result as $rslt):?>
+            <tr>
+              <td><?= $no++; ?></td>
+              <td><?= $rslt["nama_pemesan"]; ?></td>
+              <td><?= $rslt["jumlah_pesanan"]; ?></td>
+              <td><?= $rslt["alamat_pengambilan"]; ?></td>
+              <td><?= $rslt["alamat_tujuan"]; ?></td>
+              <td><?= $rslt["jenis_pengiriman"]; ?></td>
+              <td><?= $rslt["tanggal_sampai"]; ?></td>
+              <td>                
+                  <?php if($rslt["status_pengiriman"] == "On Progress"):?>
+                    <div class="badge badge-success text-wrap">
+                      On Progress
+                    </div>
+                  <?php else:?>
+                    <div class="badge badge-success text-wrap">
+                      Searching Driver
+                    </div>
+                  <?php endif;?>
+              </td>
+            </tr>
+            <?php endforeach;?>
+          </tbody>
+        </table>
+
+      </div>  
+    </main>
+      
 
 
     <script src="libraries/jquery/jquery.js"></script>
